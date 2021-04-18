7a4e7337d5ee3e5420c7d0d82529e82ac64751fe
stormshadowcr7 <42135765+stormshadowcr7@users.noreply.github.com>
2021-04-18 23:40:19 +0530

    Update Assignment12.sh

--- Unix command line and Shell scripting with bash/Shell scripting with bash/Assignment12.sh
+++ Unix command line and Shell scripting with bash/Shell scripting with bash/Assignment12.sh
@@ -14,10 +14,10 @@ read -p "Enter a number between 1 and 3:" num
 if [[ 0 -lt $num ]]; then
       
 	if [[ $num -lt 4 ]]; then
-	     echo "right h"
+	     echo "Right choice"
          
         else
-             echo "between 1 and 3 dumbass"
+             echo "Please Enter between 1 and 3"
              echo $num > /dev/null
         fi
  fi
